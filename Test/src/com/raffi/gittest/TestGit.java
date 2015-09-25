5b2b62ad1b621644ffd8d95046650cff84e32fb0
mohammedraffishaik <mohammedraffishaik@yahoo.com>
2015-09-26 06:59:51 +0800

    Add new output First time change.

--- Test/src/com/raffi/gittest/TestGit.java
+++ Test/src/com/raffi/gittest/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Raffi, Welcome to Git!");
+		System.out.println("Raffi, Welcome to Git! First Push");
 	}
 
 }
