8e7f9792c9e4d77f5ccdf1ceba6586d42712eaeb
mohammedraffishaik <mohammedraffishaik@yahoo.com>
2015-09-26 11:57:41 +0800

    Fetch and Push

--- Test/src/com/raffi/gittest/TestGit.java
+++ Test/src/com/raffi/gittest/TestGit.java
@@ -5,6 +5,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Raffi, Welcome to Git!");
 		System.out.println("Raffi, Welcome to Git! First Push");
+		
+		System.out.println("Raffi, Welcome to Git! Fetch and Push");
 	}
 
 }
